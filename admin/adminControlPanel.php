0b17b0515b9cb1e5febaebd500dfbfa06e4320a8
Constantine Spencer <constantinos-@hotmail.com>
2017-03-20 14:35:59 +0200

    More

--- admin/adminControlPanel.php
+++ admin/adminControlPanel.php
@@ -1,57 +1,12 @@
 <h4>Admin Panel</h4>
 
+<div class="row" style="padding-bottom: 25px;">
+    <?php include 'admin/includes/adminCockpit.php'; ?>
+</div>
+
 <div class="row"> <!-- Search Row - Constantine -->
     
-    
-    
-    <div class="box"> <!-- Reservations Search box - Constantine -->
-        <div class="col-md-6">
-            <h5>Search Reservations</h5>
-            <form class="form-group" method="GET">
-                <input type="hidden" name="panel" value="viewReservations">
-                <input type="hidden" name="page" value="1">
-                <div class="col-md-12">Name or Surname: <input class="form-control form-inline" type="text" name="firstNameOrLastNameSearched" placeholder="At least three letters"></div>
-                <div class="col-md-12">Date: <input class="form-control form-inline" type="date" name="dateSearched"></div>
-                <div class="col-md-12">Reservation Status<select name="statusSearched" class="form-control form-inline">
-                        <option value="default" selected>All</option>
-                        <?php
-                        include "./functions/dbcon.php";
-                        $sql = "SELECT `B_STATUS_ID`,`B_STATUS_NAME` FROM `booking_status`"; // Collect booking_statuses options from database - Constantine
-                        $result = $mysqli->query($sql);
-                        if ($result->num_rows > 0) {
-                            while ($row = $result->fetch_assoc()) {
-                                if (($row['B_STATUS_ID'] !== '6') && ($row['B_STATUS_ID'] !== '3'))  {
-                                    echo "<option value=" . $row['B_STATUS_ID'] . ">" . $row['B_STATUS_NAME'] . "</option>";
-                                }
-                            }
-                        }
-                        $mysqli->close();
-                        ?>
-                    </select>
-                </div>
-                <div class="col-md-4 col-md-offset-4" style="padding: 20px 0px;"><button type="submit" name="bookingSearched" value="1" class="btn btn-info">Search Reservation</button></div>
-
-
-            </form>
-            </div> <!-- End of Reservations column -->
-
-    
-    
-    
-
-        <div class="col-md-6">
-            <h5>Search Users</h5>
-            <form class="form-group" method="GET">
-                <input type="hidden" name="panel" value="members">
-                <input type="hidden" name="page" value="1">
-                <div class="col-md-12">Name, Surname, Username, E-mail: <input class="form-control form-inline" type="text" name="nameSearched" placeholder="At least three letters"></div>
-                <div class="col-md-4 col-md-offset-4" style="padding: 20px 0px;"><button type="submit" name="userSearched" value="1" class="btn btn-info">Search User</button></div>
-
-
-            </form>
-        </div> <!-- End of Reservations Search box -->
-    </div> <!-- End of Search box -->
-    
+<?php include 'admin/includes/searchboxes.php'; ?> 
     
 </div> 
 
---
+++ functions/fetchReservations.php
@@ -0,0 +1,31 @@
+<?php include 'functions/dbcon.php';
+
+// Fetch Pending and Todau's reservations with two queries - Constantinos
+$today = date("Y-m-d");
+$pendingReservations = ""; //Initializing variable to bind pending reservations
+$todaysReservations = ""; //Initializing variable to bind today's reservations
+
+$sql = "SELECT COUNT(`BOOKING_ID`) FROM `booking` WHERE `booking_status_B_STATUS_ID` = '1'";
+$result = $mysqli->query($sql);
+
+    
+        while ($row = $result->fetch_row()) {
+        $pendingReservations =  $row['0'];
+    }
+    
+$sql1 = "SELECT COUNT(`BOOKING_ID`) FROM `booking` WHERE `BOOKING_DATE` = '". $today ."' ";
+
+$result1 = $mysqli->query($sql1);
+
+    
+        while ($row = $result1->fetch_row()) {
+        $todaysReservations =  $row['0'];
+    }
+    
+$result->close();
+$result1->close();
+$mysqli->close();
+
+// End of Fetch Pending and Todau's reservations with two queries
+
+?>
