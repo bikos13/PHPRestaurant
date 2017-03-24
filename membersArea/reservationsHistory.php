ba3a768b2a00fd061608319a5a1e767bf8c2d0df
Constantine Spencer <constantinos-@hotmail.com>
2017-03-24 03:54:47 +0200

    fixed query errors

--- membersArea/reservationsHistory.php
+++ membersArea/reservationsHistory.php
@@ -36,7 +36,7 @@ function pagBut($page_number, $buttontext) {
 //Main query to get users reservation history - Constantine ==================================================
 //============================================================================================================
 
-$sql = "SELECT * FROM booking, booking_status WHERE ((BOOKING_DATE < CURDATE() AND USERS_USER_ID = " . $_SESSION['userdata']['userid'] . ") OR booking_status_B_STATUS_ID = '3') AND booking.booking_status_B_STATUS_ID = booking_status.B_STATUS_ID ORDER BY BOOKING_ID DESC LIMIT " . $pr . "," . $rowsperpage;
+$sql = "SELECT * FROM booking, booking_status WHERE USERS_USER_ID = " . $_SESSION['userdata']['userid'] . " AND booking.booking_status_B_STATUS_ID = booking_status.B_STATUS_ID ORDER BY BOOKING_ID DESC LIMIT " . $pr . "," . $rowsperpage;
 $result = $mysqli->query($sql);
 
 if ($result->num_rows > 0) {
@@ -72,28 +72,12 @@ if ($result->num_rows > 0) {
         } else {
             echo "0 results";
         }
-//if ($result->num_rows > 0) {
-//    echo "<table class='table table-bordered' style='margin:0 !important;'><tr><thead><th>ID</th><th>Date</th><th>Time</th><th>Table Size</th><th>Smoking area</th><th>Status</th></thead></tr>";
-//    // output data of each row
-//    while ($row = $result->fetch_assoc()) {
-//        (($row['SMOKING_BOOL'] === '1') ? "Smoking" : "<strong>Non</strong>-Smoking")
-//
-//          . $smokers . "</td><td>" . $row["B_STATUS_NAME"] . "</td></tr>";
-//    }
-//    echo "</table>";
-//} else {
-//    echo "0 results";
-//}
-
-//End of Main query to get users reservation history =========================================================
-//============================================================================================================
-
 
 //============================================================================================================
 //Quering result pages number to utilize pagination button (when to show or hide) - Constantine ==============
 //============================================================================================================
 
-$sql_pagin = "SELECT BOOKING_ID, BOOKING_DATE, BOOKING_TIME, BOOKING_SIZE, SMOKING_BOOL FROM booking WHERE USERS_USER_ID =" . $_SESSION['userdata']['userid']; //query to return pagination size
+$sql_pagin = "SELECT * FROM booking, booking_status WHERE USERS_USER_ID = " . $_SESSION['userdata']['userid'] . " AND booking.booking_status_B_STATUS_ID = booking_status.B_STATUS_ID ORDER BY BOOKING_ID"; //query to return pagination size
 $result1 = $mysqli->query($sql_pagin);
 $count_results = mysqli_num_rows($result1);
 $check_pages_size = $pr + $rowsperpage;
